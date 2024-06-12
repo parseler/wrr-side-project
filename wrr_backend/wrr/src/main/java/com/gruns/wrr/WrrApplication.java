ebc3bf1d83e1e4ad1bd57dfa58193d6872b78e62
parseley <gruns0989@naver.com>
2024-06-12 14:53:52 +0900

    Initial commit

---
+++ wrr_backend/wrr/src/main/java/com/gruns/wrr/WrrApplication.java
@@ -0,0 +1,13 @@
+package com.gruns.wrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrrApplication.class, args);
+	}
+
+}
---
+++ wrr_backend/wrr/src/test/java/com/gruns/wrr/WrrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruns.wrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
