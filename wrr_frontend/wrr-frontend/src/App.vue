b3ca51f330e6f0f6ca59d55a3a8fbaeb4c1786ce
parseley <gruns0989@naver.com>
2024-06-12 22:42:16 +0900

    feat: Add login button

---
+++ wrr_frontend/wrr-frontend/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <a href="http://localhost:8080/oauth2/authorization/google">Login with Google</a>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
