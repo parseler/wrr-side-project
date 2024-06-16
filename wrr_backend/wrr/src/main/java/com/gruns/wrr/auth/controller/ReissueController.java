6b8cfe2c3b383c51ad7a6c31116603550f00954a
parseley <gruns0989@naver.com>
2024-06-16 21:02:50 +0900

    refactor: Add TokenService

--- wrr_backend/wrr/src/main/java/com/gruns/wrr/auth/controller/ReissueController.java
+++ wrr_backend/wrr/src/main/java/com/gruns/wrr/auth/controller/ReissueController.java
@@ -1,7 +1,7 @@
 package com.gruns.wrr.auth.controller;
 
-import com.gruns.wrr.auth.entity.RefreshTokenEntity;
 import com.gruns.wrr.auth.repository.RefreshTokenRepository;
+import com.gruns.wrr.auth.service.TokenService;
 import com.gruns.wrr.auth.util.JwtUtil;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.Cookie;
@@ -11,17 +11,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-
 @RestController
 public class ReissueController {
 
     private final JwtUtil jwtUtil;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final TokenService tokenService;
 
-    public ReissueController(JwtUtil jwtUtil, RefreshTokenRepository refreshTokenRepository) {
+    public ReissueController(JwtUtil jwtUtil, RefreshTokenRepository refreshTokenRepository, TokenService tokenService) {
         this.jwtUtil = jwtUtil;
         this.refreshTokenRepository = refreshTokenRepository;
+        this.tokenService = tokenService;
     }
 
     @PostMapping("/reissue")
@@ -61,34 +61,14 @@ public class ReissueController {
         String role = jwtUtil.getRole(refreshToken);
 
         String reissuedAccessToken = jwtUtil.createJwt("access", username, role, 60 * 10 * 1000L);
-        String reissuedRefreshToken = jwtUtil.createJwt("refresh", username, role, 24 * 60 * 60 * 1000L);
+        String reissuedRefreshToken = jwtUtil.createJwt("refresh", username, role, 60 * 60 * 1000L);
 
         refreshTokenRepository.deleteByToken(refreshToken);
-        addRefreshTokenEntity(username, reissuedRefreshToken, 24 * 60 * 60 * 1000L);
+        tokenService.addRefreshTokenEntity(username, reissuedRefreshToken, 60 * 60 * 1000L);
 
         response.setHeader("accessToken", reissuedAccessToken);
-        response.addCookie(createCookie("refreshToken", reissuedRefreshToken));
+        response.addCookie(tokenService.createCookie("refreshToken", reissuedRefreshToken));
 
         return ResponseEntity.ok().build();
-    }
-
-    private Cookie createCookie(String key, String value) {
-
-        Cookie cookie = new Cookie(key, value);
-        cookie.setMaxAge(24*60*60);
-        //cookie.setSecure(true); https 에서만 허용
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-
-        return cookie;
-    }
-
-    private void addRefreshTokenEntity(String username, String token, Long expiredMs) {
-
-        Date date = new Date(System.currentTimeMillis() + expiredMs);
-
-        RefreshTokenEntity refreshTokenEntity = new RefreshTokenEntity(username, token, date.toString());
-
-        refreshTokenRepository.save(refreshTokenEntity);
     }
 }
