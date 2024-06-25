12367290467cbaf6a3fc31dc267f5d8a531d575d
parseley <gruns0989@naver.com>
2024-06-25 23:11:34 +0900

    docs: Add movement dump

--- wrr_backend/wrr/src/main/resources/sql/wrrdb_query.sql
+++ wrr_backend/wrr/src/main/resources/sql/wrrdb_query.sql
@@ -7,10 +7,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema wrrdb
 -- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema wrrdb
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `wrrdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `wrrdb` ;
 
