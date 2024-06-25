523d047ad3c370b68b433d08fa91cbfb4b91e4a4
parseler <162273119+parseler@users.noreply.github.com>
2024-06-25 18:20:16 +0900

    feat: Add WodController, WodService

---
+++ wrr_backend/wrr/src/main/java/com/gruns/wrr/wod/controller/WodController.java
@@ -0,0 +1,11 @@
+package com.gruns.wrr.wod.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WodController {
+    
+    // WOD entity, dto 만들기
+
+
+}
---
+++ wrr_backend/wrr/src/main/java/com/gruns/wrr/wod/service/WodService.java
@@ -0,0 +1,5 @@
+package com.gruns.wrr.wod.service;
+
+public interface WodService {
+
+}
