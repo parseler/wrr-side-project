aba377c8f8f0dcdb8545cd9c542716f14ba9c21b
parseler <162273119+parseler@users.noreply.github.com>
2024-06-30 16:50:56 +0900

    refactor: Remove MovementController, BoxController

--- wrr_backend/wrr/src/main/java/com/gruns/wrr/wod/controller/WodController.java
+++ wrr_backend/wrr/src/main/java/com/gruns/wrr/wod/controller/WodController.java
@@ -1,5 +1,8 @@
 package com.gruns.wrr.wod.controller;
 
+import com.gruns.wrr.wod.dto.BoxDto;
+import com.gruns.wrr.wod.dto.MovementDto;
+import com.gruns.wrr.wod.dto.TypeDto;
 import com.gruns.wrr.wod.dto.WodDto;
 import com.gruns.wrr.wod.service.WodService;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/wod")
@@ -22,6 +27,28 @@ public class WodController {
     public ResponseEntity<?> saveWod(@RequestBody WodDto wodDto) {
         System.out.println("wodDto = " + wodDto);
 
+        wodService.saveWod(wodDto);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/movement/list")
+    public ResponseEntity<?> getMovementList() {
+
+        List<MovementDto> list = wodService.getMovementList();
+        return ResponseEntity.ok(list);
+    }
+
+    @PostMapping("/box/list")
+    public ResponseEntity<?> getBoxList() {
+        List<BoxDto> list = wodService.getBoxList();
+
+        return ResponseEntity.ok(list);
+    }
+
+    @PostMapping("/type/list")
+    public ResponseEntity<?> getTypeList() {
+        List<TypeDto> list = wodService.getTypeList();
+
+        return ResponseEntity.ok(list);
     }
 }
