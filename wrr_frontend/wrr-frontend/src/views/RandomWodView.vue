01740e427637c7bf658edfa10924bd4c31ccf3a6
parseley <gruns0989@naver.com>
2024-06-30 22:57:07 +0900

    test: 화면 구성 예시

--- wrr_frontend/wrr-frontend/src/views/RandomWodView.vue
+++ wrr_frontend/wrr-frontend/src/views/RandomWodView.vue
@@ -1,13 +1,45 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-    <div>
-        
+  <!-- component -->
+  <div class="mt-12 flex justify-center">
+    <div
+      class="relative flex w-full max-w-[48rem] flex-row rounded-xl bg-back bg-clip-border text-logo shadow-lg text-center"
+    >
+      <div class="p-6 flex flex-col items-center justify-center w-full">
+        <h4
+          class="mb-4 block font-sans text-2xl font-semibold leading-snug tracking-normal antialiased"
+        >
+          DU_WBS_DP
+        </h4>
+        <p
+          class="mb-1 block font-sans text-2xl font-semibold leading-relaxed antialiased text-center"
+        >
+          Team of 2
+        </p>
+        <p
+          class="mb-1 block font-sans text-2xl font-semibold leading-relaxed antialiased text-center"
+        >
+          3 Rounds For Time
+        </p>
+        <p
+          class="mb-1 block font-sans text-2xl font-semibold leading-relaxed antialiased text-center"
+        >
+          60 Double Unders
+        </p>
+        <p
+          class="mb-1 block font-sans text-2xl font-semibold leading-relaxed antialiased text-center"
+        >
+          40 WallBallShots
+        </p>
+        <p
+          class="mb-1 block font-sans text-2xl font-semibold leading-relaxed antialiased text-center"
+        >
+          20 Devil Presses
+        </p>
+      </div>
     </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
