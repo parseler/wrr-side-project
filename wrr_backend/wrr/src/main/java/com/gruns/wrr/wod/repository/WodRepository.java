dec3c8c5f2bd0347dbf5002fc784360d840e3cb8
parseley <gruns0989@naver.com>
2024-07-27 22:09:49 +0900

    refactor: Modify to mongodb repository

--- wrr_backend/wrr/src/main/java/com/gruns/wrr/wod/repository/WodRepository.java
+++ wrr_backend/wrr/src/main/java/com/gruns/wrr/wod/repository/WodRepository.java
@@ -1,13 +1,7 @@
 package com.gruns.wrr.wod.repository;
 
-import com.gruns.wrr.wod.Entity.WodEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+import com.gruns.wrr.wod.Entity.Wod;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-@Repository
-public interface WodRepository extends JpaRepository<WodEntity, Long> {
-
-    @Query(value = "select * from wod order by rand() limit 1", nativeQuery = true)
-    WodEntity findRandomWod();
+public interface WodRepository extends MongoRepository<Wod, String> {
 }
