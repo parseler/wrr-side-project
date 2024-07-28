8e6a1d34e2e41ec6231f85abf707b9de7668acee
parseley <gruns0989@naver.com>
2024-07-28 09:33:53 +0900

    feat: Add NoSuchElementException handler

--- wrr_backend/wrr/src/main/java/com/gruns/wrr/exception/ExceptionController.java
+++ wrr_backend/wrr/src/main/java/com/gruns/wrr/exception/ExceptionController.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.NoSuchElementException;
+
 @RestControllerAdvice
 @Slf4j
 public class ExceptionController {
@@ -13,5 +15,11 @@ public class ExceptionController {
     public ResponseEntity<?> handleException(Exception e) {
         log.error("Exception : ", e.getCause());
         return ResponseEntity.internalServerError().body("Error : " + e.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<?> handleNoSuchElementException(NoSuchElementException e) {
+        log.error("NoSuchElementException : ", e);
+        return ResponseEntity.notFound().build();
     }
 }
