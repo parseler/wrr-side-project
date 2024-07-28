9f336e9bb36518d1ad663f32813d3e5e33987f96
parseley <gruns0989@naver.com>
2024-07-28 09:34:13 +0900

    feat: Add Wod Entity

--- wrr_backend/wrr/src/main/java/com/gruns/wrr/wod/Entity/Wod.java
+++ wrr_backend/wrr/src/main/java/com/gruns/wrr/wod/Entity/Wod.java
@@ -1,9 +1,13 @@
 package com.gruns.wrr.wod.Entity;
 
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "wod")
+@Builder
+@Getter
 public class Wod {
 
     @Id
